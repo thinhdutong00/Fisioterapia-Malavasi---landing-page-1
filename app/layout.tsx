fdeaf25762362097f96c19142bed4019ee3053d1
Du Tong Thinh <dutongthinh@MacBook-Air-di-Thinh.local>
2026-02-24 18:16:46 +0100

    fixtitle

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Fisioterapia Malavasi | Bomporto",
+  title: "Fisioterapia Malavasi | Cavezzo",
   description: "Studio di Fisioterapia del Dott. Mirco Malavasi",
 };
 
