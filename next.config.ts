90bfef96a52ad51a5950edeed49e1724f8240683
Du Tong Thinh <dutongthinh@MacBook-Air-di-Thinh.local>
2026-02-24 21:22:28 +0100

    gitfix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
