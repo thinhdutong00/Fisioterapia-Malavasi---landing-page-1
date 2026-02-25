62594192c4e7d5cf789f3932b2af4dd2f58c79db
Du Tong Thinh <dutongthinh@MacBook-Air-di-Thinh.local>
2026-02-25 17:19:59 +0100

    git

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-// Aggiunto l'import per gestire correttamente lo script di Clarity
+// Aggiunto l'import per gestire correttamente lo script di Clarity e GTM
 import Script from "next/script";
 
 const geistSans = Geist({
@@ -27,6 +27,17 @@ export default function RootLayout({
   return (
     <html lang="it">
       <head>
+        {/* Google Tag Manager - Script principale */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WRNZP7MS');
+          `}
+        </Script>
+
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -43,23 +54,31 @@ export default function RootLayout({
           }}
         />
         
+        <link rel="preconnect" href="https://raw.githubusercontent.com" />
+        <link rel="dns-prefetch" href="https://raw.githubusercontent.com" />
 
-<link rel="preconnect" href="https://raw.githubusercontent.com" />
-  <link rel="dns-prefetch" href="https://raw.githubusercontent.com" />
-
-
-{/* Blocco Microsoft Clarity ottimizzato */}
-<Script id="microsoft-clarity" strategy="lazyOnload">
-  {`
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "vm4wfzivpa");
-  `}
-</Script>
+        {/* Blocco Microsoft Clarity ottimizzato */}
+        <Script id="microsoft-clarity" strategy="lazyOnload">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vm4wfzivpa");
+          `}
+        </Script>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WRNZP7MS"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        
         {children}
       </body>
     </html>
